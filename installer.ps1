460cd0a08316ade38b6d5cd565d3bd8c6caa4456
sumce <info@aosa.me>
2026-08-28 19:16:59 +0800

    chore: keep installer ps1 UTF-8 with BOM

--- installer.ps1
+++ installer.ps1
@@ -1,4 +1,4 @@
-[CmdletBinding()]
+﻿[CmdletBinding()]
 param(
     [ValidateSet("Menu", "InstallUser", "InstallAll", "VerifyUser", "VerifyAll", "VerifyPackage", "UninstallUser", "UninstallAll")]
     [string]$Mode = "Menu",
